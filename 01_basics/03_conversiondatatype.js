13b3ab54c16c98de2a6af7b8db8a4a860c8c177b
Ankush9354 <ankushsharma03059@gmail.com>
2025-08-29 04:59:24 +0000

    done

---
+++ 01_basics/03_conversiondatatype.js
@@ -0,0 +1,30 @@
+let  score = "35abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueinnum = Number(score)
+
+console.log(typeof valueinnum)
+console.log( valueinnum);
+
+// "33" => 33
+// "33abc" =>  NaN
+// True => 1 ,false =>0
+
+let isloggedin= ""
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+// 1 => true, 0 => false
+// "ankush" => true 
+// "" => false
+
+let num = 3546
+console.log(typeof num);
+
+
+let stringnum=String(num)
+console.log(typeof stringnum);
+
+console.log(stringnum);
